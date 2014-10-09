c43b27be9ec8c9335841f27df99679c44884129c
Brant Wellman <brantwellman@gmail.com>
2014-10-09 15:11:56 +0000

    Syle adjs to header and footer, added viewport meta-tag

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,6 +1,7 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
   config.input_class = "form-control"
+  config.label_text = lambda { |label, required| "#{label}" }
   
   # Wrappers are used by the form builder to generate a
   # complete input. You can remove any component from the
