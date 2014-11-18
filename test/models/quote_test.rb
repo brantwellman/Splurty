f74e6c76d2bdbb71bca7c467852eaa18418db765
Brant Wellman <brantwellman@gmail.com>
2014-11-18 21:01:11 +0000

    TDD - Added Quote unique tag to quote display

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
-   end
+   
+	test "unique_tag" do
+		quote = Quote.create(:author => "Brant Wellman", :saying => "The sky is bluer than the water.")		
+		expected = 'BW#' + quote.id.to_s
+		actual = quote.unique_tag
+		assert_equal expected, actual
+	end
+
+
 end
