d2ab7c44f62c275a2160fa09b907542da3db14ab
Brant Wellman <brantwellman@gmail.com>
2014-11-18 22:12:25 +0000

    Fixed deprecation error

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module Splurty
 
     # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .woff2 .ttf )
+    config.i18n.enforce_available_locales = false
   end
 end
